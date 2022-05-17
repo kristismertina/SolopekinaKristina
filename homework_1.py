38df0d18fc9e493e83adcc9c37a90c855ffaf460
user <Kris>
2022-05-17 23:21:53 +0300

    first

---
+++ homework_1.py
@@ -0,0 +1,7 @@
+my_name = "Kristina"
+favorite_color = "black"
+favorite_group = "queen"
+favorite_movie = "Pirates of the Caribbean"
+
+
+print("Меня зовут:", my_name, "Моя любимая группа:", favorite_group, "Мой любимый цвет:", favorite_color, "Мой любимый фильм:", favorite_movie, sep='\n')
